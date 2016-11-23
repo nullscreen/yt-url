df031185636f0d8ae26be37362dbe84399aa08b2
claudiob <claudiob@inventati.org>
2016-11-23 09:38:34 -0800

    Extract code into spec/support

---
+++ spec/support/server.rb
@@ -0,0 +1,12 @@
+RSpec.configure do |config|
+  config.before :all, server: true do
+    Yt.configuration.api_key = ENV['YT_SERVER_API_KEY']
+    Yt.configuration.client_id = ''
+    Yt.configuration.client_secret = ''
+  end
+
+  $existing_channel_id   = 'UCxO1tY8h1AhOz0T4ENwmpow'
+  $existing_video_id     = 'gknzFj_0vvY'
+  $unknown_video_id      = 'invalid-id-'
+  $unknown_text          = 'not-really-anything---'
+end
